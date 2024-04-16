6948d1851b054341b59b62d9d82ad999e030176e
Kirill Wysocki <kirill_260203@mail.ru>
2024-04-16 23:35:48 +0600

    Добавьте файлы проекта.

---
+++ Основы программирования C# Часть 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Основы_программирования_C__Часть_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
