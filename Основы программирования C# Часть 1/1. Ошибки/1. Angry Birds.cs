a829aeb503bfd6825ade2cd7ee804eac7b8aa5ba
Kirill Wysocki <kirill_260203@mail.ru>
2024-04-16 23:42:04 +0600

    1

---
+++ Основы программирования C# Часть 1/1. Ошибки/1. Angry Birds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AngryBirds
+{
+    public static class AngryBirdsTask
+    {
+        public static double FindSightAngle(double v, double distance)
+        {
+            var g = 9.8;
+            return 0.5 * Math.Asin(distance * g / (v * v));
+        }
+    }
+}
